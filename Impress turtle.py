767d86c1b2f7469db184c5138292ac5aa4ac6ee0
Rahul <155445391+Haru0411@users.noreply.github.com>
2024-01-28 03:04:21 +0530

    Update Impress turtle.py

--- Impress turtle.py
+++ Impress turtle.py
@@ -137,8 +137,8 @@ def write(message,pos):
 
        style=("Stencil Std", 18, "italic")
 
-       t.write('AISHTERU', font=style)
+       t.write(message , font=style)
 
-write('PANDA',(-85,-145))
+write('PANDA🐼',(-85,-145))
 
 turtle.done()
